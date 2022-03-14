fef2a21232a3086a9f78124e0b5d02507775613d
ptnguyen617 <ptnguyen617@yahoo.com>
2022-03-14 19:54:50 -0400

    Updating code for heroku

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const User = require("../models/user");
+const User = require("../models/User");
 const bcrypt = require("bcrypt");
 const router = express.Router();
 
